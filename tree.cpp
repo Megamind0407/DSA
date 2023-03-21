a8c3d536f1083f7eff790e5ec47fe5d82b05b3fb
Megamind0407 <112895377+Megamind0407@users.noreply.github.com>
2023-03-21 19:53:35 +0530

    Update tree.cpp

--- tree.cpp
+++ tree.cpp
@@ -44,6 +44,76 @@ class stack1
         }
 
 };
+class stack2
+{
+    public:
+        node *arr[20];
+        int top;
+        stack2()
+        {
+            top=-1;
+        }
+        void push(node *temp){
+            top++;
+            arr[top]=temp;
+        }
+        int empty()
+        {
+            if(top==-1)
+            {
+                return 1;
+            }
+            else{
+                return 0;
+            }
+        }
+        node *pop()
+        {
+            node *p=arr[top];
+            top--;
+            return p;
+        }
+};
+class queue1{
+    public:
+        int r,f;
+        node* arr[10];
+        queue1(){
+            r=f=-1;
+        }
+        int empty()
+        {
+            if(r==-1 && f==-1){
+                return 1;
+            }
+            else{
+                return 0;
+            }
+        }
+        void insert(node* p)
+        {
+            if(empty()==1){
+                r=f=0;
+                arr[r]=p;
+            }
+            else{
+                r=r+1;
+                arr[r]=p;
+            }
+        }
+        node* remove()
+        {
+            node* temp=arr[f];
+            if(r==f)
+            {
+                r=f=-1;
+            }
+            else{
+                f=f+1;
+            }
+            return temp;
+        }
+};
 class tree
 {
     public:
@@ -51,7 +121,7 @@ class tree
         node *create()
         {
             string key;
-            cout<<"Enter a book(if not write NULL) :";
+            cout<<"\nEnter a book(if not write NULL) :";
             cin>>key;
 
             if(key=="NULL")
@@ -71,7 +141,7 @@ class tree
         {
             if(root!=NULL)
             {
-                cout<<root->data<<endl;
+                cout<<root->data<<" ";
                 preorder(root->left);
                 preorder(root->right);
             }
@@ -85,7 +155,7 @@ class tree
             if(root!=NULL)
             {
                 inorder(root->left);
-                cout<<root->data<<endl;
+                cout<<root->data<<" ";
                 inorder(root->right);
             }
             else{
@@ -103,7 +173,7 @@ class tree
             else{
                 postorder(root->left);
                 postorder(root->right);
-                cout<<root->data<<endl;
+                cout<<root->data<<" ";
             }
         }
         void nonrec_inorder(node *t)
@@ -116,7 +186,7 @@ class tree
                     t=t->left;
                 }
                 t=s.pop();
-                cout<<t->data<<endl;
+                cout<<t->data<<" ";
                 t=t->right;
             }
         }
@@ -126,7 +196,7 @@ class tree
             while(t!=NULL || s.empty()!=1)
             {
                 while(t!=NULL){
-                    cout<<t->data<<endl;
+                    cout<<t->data<<" ";
                     s.push(t);
                     t=t->left;
                 }
@@ -134,9 +204,51 @@ class tree
                 t=t->right;
             }
         }
+        void nonrec_postorder(node* t){
+            stack1 s;
+            stack2 s1;
+            node* temp;
+            if(t==NULL)
+            {
+                return;
+            }
+            s.push(t);
+            while(s.empty()!=1){
+                temp=s.pop();
+                s1.push(temp);
+                if(temp->left!=NULL)
+                    s.push(temp->left);
+                if(temp->right!=NULL)
+                    s.push(temp->right);
+            }
+            while(s1.empty()!=1){
+                temp=s1.pop();
+                cout<<temp->data<<" ";
+            }
+        }
+        void BFS(node* t)
+        {
+            queue1 q;
+            q.insert(t);
+            cout<<t->data<<" ";
+            while(q.empty()!=1)
+            {
+                t=q.remove();
+                if(t->left!=NULL)
+                {
+                    cout<<t->left->data<<" ";
+                    q.insert(t->left);
+                }
+                if(t->right!=NULL)
+                {
+                    cout<<t->right->data<<" ";
+                    q.insert(t->right);
+                }
+            }
+        }
         void menu()
         {
-            cout<<"1.Insert\n2.Preorder\n3.Inorder\n4.Postorder\n5.Non-Rec Inorder\n6.Non-Rec Preorder"<<endl;
+            cout<<"\n1.Insert\n2.Preorder\n3.Inorder\n4.Postorder\n5.Non-Rec Inorder\n6.Non-Rec Preorder\n7.Non-Rec Postorder\n8.BFS"<<endl;
         }
 };
 int main()
@@ -168,8 +280,13 @@ int main()
             case 6:
                 t.nonrec_preorder(t.root);
                 break;
+            case 7:
+                t.nonrec_postorder(t.root);
+                break;
+            case 8:
+                t.BFS(t.root);
+                break;
             default:
-                cout<<"Invalid choice";
                 exit(1);
         }
     }
